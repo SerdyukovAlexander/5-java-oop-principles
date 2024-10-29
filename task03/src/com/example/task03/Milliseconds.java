ef00dc68206ab54d3766f844c4719d52695440be
Serdyukov Alexander <30432511+SerdyukovAlexander@users.noreply.github.com>
2024-10-29 16:25:07 +0500

    задание 3

--- task03/src/com/example/task03/Milliseconds.java
+++ task03/src/com/example/task03/Milliseconds.java
@@ -3,26 +3,36 @@ package com.example.task03;
 /**
  * Интервал в миллисекундах
  */
-public class Milliseconds implements TimeUnit {
-
+public class Milliseconds implements TimeUnit
+{
     private final long amount;
 
-    public Milliseconds(long amount) {
+    public Milliseconds(long amount)
+    {
         this.amount = amount;
     }
 
     @Override
-    public long toMillis() {
+    public long toMillis()
+    {
         return amount;
     }
 
     @Override
-    public long toSeconds() {
-        return amount / 1000;
+    public long toSeconds()
+    {
+        return Math.round(amount / 1000f);
     }
 
     @Override
-    public long toMinutes() {
-        return amount / 1000 * 60;
+    public long toMinutes()
+    {
+        return Math.round(amount / 60000f);
+    }
+
+    @Override
+    public long toHours()
+    {
+        return Math.round(amount / 3600000f);
     }
 }
